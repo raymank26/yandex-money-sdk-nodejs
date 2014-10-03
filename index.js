8576c4069d7de0a0bce0795c5a9561c5622574bf
Your Name <Rayman.k26@gmail.com>
2014-10-03 18:23:01 +0400

    more methods and tests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var wallet = require("./lib/wallet.js");
+var externalPayment = require("./lib/external_payment.js");
 
 module.exports = {
-  Wallet: wallet.Wallet
+  Wallet: wallet.Wallet,
+  ExternalPayment: externalPayment.ExternalPayment
 };
--- test/test.js
+++ test/test.js
@@ -2,29 +2,32 @@ var assert = require("assert");
 var yandex_money = require("../index");
 
 var Wallet = yandex_money.Wallet;
-
-function addResponse(wallet, response, callback) {
-  Wallet.sendUnauthenticatedRequest = function(params, callback) {
-    return response;
-  };
-}
+var ExternalPayment = yandex_money.ExternalPayment;
 
 describe('Wallet', function(){
 
-  describe('#buildObtainTokenUrl', function(){
+  describe('#utils', function(){
     it('should return auth url for browser redirect', function(){
       var clientId = "clientId";
       var redirectURI = "redirectURI";
       var scope = ['account-info', 'operation-history'];
       var url = Wallet.buildObtainTokenUrl(clientId, redirectURI, scope);
     });
+
+    it("should exchange code to access token(fake)", function(done) {
+      Wallet.getAccessToken("client id", "code", "redirect uri", null,
+        function (error, data, response) {
+          assert.equal(response.statusCode, 200);
+          done();
+        });
+    });
   });
 
   describe("#accountInfo", function () {
     it("should return account information", function (done) {
       var wallet = new Wallet(accessToken);
-      wallet.accountInfo(function myCallback(error, data) {
-        //console.log(data);
+      wallet.accountInfo(function myCallback(error, data, response) {
+        assert.equal(response.statusCode, 200);
         done();
       });
     });
@@ -43,21 +46,7 @@ describe('Wallet', function(){
   });
 
   describe("#operationDetails", function () {
-    it("should return operation history", function (done) {
-      var wallet = new Wallet(accessToken);
-      wallet.operationDetails("some operation id",
-          function myCallback(error, data, response) {
-        //console.log(data);
-        // yandex api always returns 200 if request is correct
-        assert.equal(response.statusCode, 200);
-        done();
-      });
-    });
-  });
-
-
-  describe("#operationDetails", function () {
-    it("should return operation history", function (done) {
+    it("should return operation history(fake)", function (done) {
       var wallet = new Wallet(accessToken);
       wallet.operationDetails("some operation id",
           function myCallback(error, data, response) {
@@ -104,6 +93,40 @@ describe('Wallet', function(){
     });
   });
 
+  describe("#transfer", function () {
+
+    it("should accept incoming transfer(fake)", function (done) {
+      var wallet = new Wallet(accessToken);
+      wallet.incomingTransferAccept("some operation id", null,
+        function myCallback(error, data, response) {
+          //console.log(data);
+          assert.equal(response.statusCode, 200);
+          done();
+        });
+    });
+
+    it("should reject incoming transfer(fake)", function (done) {
+      var wallet = new Wallet(accessToken);
+      wallet.incomingTransferReject("some operation id",
+        function myCallback(error, data, response) {
+          assert.equal(response.statusCode, 200);
+          done();
+        });
+    });
+    
+  });
 });
 
+describe("External payment", function () {
+
+  describe("#externalPayment", function () {
+    it("should request external payment", function(done) {
+      ExternalPayment.getInstanceId(clientId, function(error, data) {
+        assert.equal(data.status, "success");
+        done();
+      });
+    });
+  });
+  
+});
 
