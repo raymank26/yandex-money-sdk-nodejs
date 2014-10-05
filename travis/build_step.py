dd8a329d00d5cfd8d64b0476156523fed0cce0f7
Your Name <Rayman.k26@gmail.com>
2014-10-05 22:36:12 +0400

    build fix

--- travis/build_step.py
+++ travis/build_step.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 import os
+import sys
 
 if "yandex-money" in os.environ['TRAVIS_REPO_SLUG']:
     encrypted_filename = "constants.js.enc.yamoney"
 else:
     encrypted_filename = "constants.js.enc"
 
-os.system("openssl aes-256-cbc -K {} -iv"
-          " {} -in {}"
-          "-out test/constants.js -d".format(
-              os.environ['encrypted_bd8aa6273b70_key'],
-              os.environ['encrypted_bd8aa6273b70_iv'],
+command = "openssl aes-256-cbc -K {} -iv {} -in {} -out test/constants.js -d".format(
+              os.environ.get('encrypted_bd8aa6273b70_key', "key1"),
+              os.environ.get('encrypted_bd8aa6273b70_iv', "key2"),
               encrypted_filename)
-          )
+
+if(len(sys.argv) > 0 and sys.argv[1] == "debug"):
+    print(command)
+else:
+    os.system(command)
+
+
