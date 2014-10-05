a1d532d3c2ce8941a9ce47433713d4bd42364863
Your Name <Rayman.k26@gmail.com>
2014-10-05 22:24:44 +0400

    build step fix

--- travis/build_step.py
+++ travis/build_step.py
@@ -6,7 +6,10 @@ if "yandex-money" in os.environ['TRAVIS_REPO_SLUG']:
 else:
     encrypted_filename = "constants.js.enc"
 
-os.system("openssl aes-256-cbc -K $encrypted_bd8aa6273b70_key -iv"
-          "$encrypted_bd8aa6273b70_iv -in {}"
-          "-out test/constants.js -d".format(encrypted_filename)
+os.system("openssl aes-256-cbc -K {} -iv"
+          " {} -in {}"
+          "-out test/constants.js -d".format(
+              os.environ['encrypted_bd8aa6273b70_key'],
+              os.environ['encrypted_bd8aa6273b70_iv'],
+              encrypted_filename)
           )
